ab9aaa5232c36ebd10b8c79214e9fe9de9ae01b9
Ben Pate <ben@pate.org>
2020-09-27 20:21:03 -0600

    Removing "Autocomplete" as a top-level field.

--- form.go
+++ form.go
@@ -9,16 +9,17 @@ import (
 
 // Form defines a single form element, or a nested form layout.  It can be serialized to and from a database.
 type Form struct {
-	Kind         string                 `json:"kind"`         // The kind of form element
-	ID           string                 `json:"id"`           // DOM ID to use for this element.
-	Label        string                 `json:"label"`        // Short label to be displayed on the form element
-	Description  string                 `json:"description"`  // Longer description text to be displayed on the form element
-	CSSClass     string                 `json:"cssClass"`     // CSS Class override to apply to this widget.  This should be used sparingly
-	Autocomplete string                 `json:"autocomplete"` // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
-	Path         string                 `json:"path"`         // Path to the data value displayed in for this form element
-	Options      map[string]interface{} `json:"options"`      // Kind-specific modifiers for the form element.
-	Children     []Form                 `json:"children"`     // Array of sub-form elements that may be displayed depending on the kind.
+	Kind        string                 `json:"kind"`        // The kind of form element
+	ID          string                 `json:"id"`          // DOM ID to use for this element.
+	Label       string                 `json:"label"`       // Short label to be displayed on the form element
+	Description string                 `json:"description"` // Longer description text to be displayed on the form element
+	CSSClass    string                 `json:"cssClass"`    // CSS Class override to apply to this widget.  This should be used sparingly
+	Path        string                 `json:"path"`        // Path to the data value displayed in for this form element
+	Options     map[string]interface{} `json:"options"`     // Kind-specific modifiers for the form element.
+	Children    []Form                 `json:"children"`    // Array of sub-form elements that may be displayed depending on the kind.
 }
+
+// 	Autocomplete string `json:"autocomplete"` // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
 
 // HTML returns a populated HTML string for the provided form, schema, and value
 func (form Form) HTML(library Library, schema schema.Schema, value interface{}) (string, error) {
