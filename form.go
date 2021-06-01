46eb80020076703898d952b2b391ea04de224b0a
Ben Pate <ben@pate.org>
2021-06-01 13:55:07 -0600

    Adding UnmarshalMap function

--- form.go
+++ form.go
@@ -1,6 +1,7 @@
 package form
 
 import (
+	"github.com/benpate/convert"
 	"github.com/benpate/derp"
 	"github.com/benpate/html"
 	"github.com/benpate/schema"
@@ -17,6 +18,46 @@ type Form struct {
 	Options     map[string]string `json:"options"`     // Additional custom properties defined by individual widgets
 	Rules       map[string]string `json:"rules"`       // Visibility rules (in hyperscript) to apply to UI.
 	Children    []Form            `json:"children"`    // Array of sub-form elements that may be displayed depending on the kind.
+}
+
+// UnmarshalMap parses data from a generic structure (map[string]interface{}) into a Form record.
+func (form *Form) UnmarshalMap(data map[string]interface{}) error {
+
+	form.Path = convert.String(data["path"])
+	form.Kind = convert.String(data["kind"])
+	form.ID = convert.String(data["id"])
+	form.Label = convert.String(data["label"])
+	form.Description = convert.String(data["description"])
+	form.CSSClass = convert.String(data["cssClass"])
+
+	form.Options = make(map[string]string)
+	if options, ok := data["options"].(map[string]interface{}); ok {
+		for key, value := range options {
+			form.Options[key] = convert.String(value)
+		}
+	}
+
+	form.Rules = make(map[string]string)
+	if rules, ok := data["rules"].(map[string]interface{}); ok {
+		for key, value := range rules {
+			form.Rules[key] = convert.String(value)
+		}
+	}
+
+	if children, ok := data["children"].([]interface{}); ok {
+		form.Children = make([]Form, len(children))
+		for index, childInterface := range children {
+			if childData, ok := childInterface.(map[string]interface{}); ok {
+				var childForm Form
+				childForm.UnmarshalMap(childData)
+				form.Children[index] = childForm
+			} else {
+				return derp.New(derp.CodeInternalError, "form.UnmarshalMap", "Error parsing child form information.", childInterface)
+			}
+		}
+	}
+
+	return nil
 }
 
 // 	Autocomplete string `json:"autocomplete"` // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/benpate/compare v0.0.4
 	github.com/benpate/convert v0.9.2
 	github.com/benpate/derp v0.21.0
-	github.com/benpate/html v0.10.0
+	github.com/benpate/html v0.11.1
 	github.com/benpate/null v0.6.0
 	github.com/benpate/path v0.4.0
 	github.com/benpate/schema v0.10.2
