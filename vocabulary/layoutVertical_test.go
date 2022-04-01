46861954f97525a6af40aea025cfde787dfac113
Ben Pate <ben@pate.org>
2022-04-01 13:25:53 -0600

    Update layoutVertical_test.go
    update test cases

--- vocabulary/layoutVertical_test.go
+++ vocabulary/layoutVertical_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/benpate/derp"
 	"github.com/benpate/form"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -51,23 +52,60 @@ func TestLayoutVertical(t *testing.T) {
 	expected := `
 	<div class="layout-vertical">
 		<div class="layout-vertical-label">This is my Vertical Layout</div>
-		<div class="layout-vertical-elements">
-			<div class="layout-vertical-element">
-				<label>Name</label>
-				<input name="name" value="John Connor" type="text" maxlength="50" tabIndex="0">
-			</div>
-			<div class="layout-vertical-element">
-				<label>Email</label>
-				<input name="email" value="john@resistance.mil" type="email" minlength="10" maxlength="100" required="true" tabIndex="0">
-			</div>
-			<div class="layout-vertical-element">
-				<label>Age</label>
-				<input name="age" value="27" type="number" step="1" min="10" max="100" required="true" tabIndex="0">
-			</div>
+		<div class="layout-vertical-element">
+			<label>Name</label>
+			<input name="name" value="John Connor" type="text" maxlength="50" tabIndex="0">
+		</div>
+		<div class="layout-vertical-element">
+			<label>Email</label>
+			<input name="email" value="john@resistance.mil" type="email" minlength="10" maxlength="100" required="true" tabIndex="0">
+		</div>
+		<div class="layout-vertical-element">
+			<label>Age</label>
+			<input name="age" value="27" type="number" step="1" min="10" max="100" required="true" tabIndex="0">
 		</div>
 	</div>`
 
 	expected = strings.ReplaceAll(expected, "\n", "")
 	expected = strings.ReplaceAll(expected, "\t", "")
 	require.Equal(t, expected, html)
+}
+
+func TestRules(t *testing.T) {
+
+	library := getTestLibrary()
+	s := getTestSchema()
+
+	f := form.Form{
+		Kind:  "layout-vertical",
+		Label: "This is my Vertical Layout",
+		Children: []form.Form{
+			{
+				Kind:  "text",
+				Label: "Name",
+				Path:  "name",
+				Show:  form.Rule{Path: "rule.1", Value: "'value1'"},
+			},
+			{
+				Kind:  "text",
+				Label: "Email",
+				Path:  "email",
+				Show:  form.Rule{Path: "rule.2", Op: "in", Value: "['value1', 'value2']"},
+			},
+			{
+				Kind:  "text",
+				Label: "Age",
+				Path:  "age",
+				Show:  form.Rule{Path: "name", Value: "'John'"},
+			},
+		},
+	}
+
+	html, err := f.HTML(&library, s, nil)
+
+	assert.Nil(t, err)
+	derp.Report(err)
+
+	spew.Dump(html)
+
 }
