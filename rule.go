452b689ad9f49ad59ca89f13dbbccfa723639380
Ben Pate <ben@pate.org>
2022-06-10 07:47:42 -0600

    Update rule.go
    - temporarily remove visibility rules

--- rule.go
+++ rule.go
@@ -1,7 +1,5 @@
 package form
 
-import "strings"
-
 type Rule struct {
 	Path  string `json:"path"`
 	Op    string `json:"op"`
@@ -16,22 +14,23 @@ func (rule Rule) Operator() string {
 	return rule.Op
 }
 
-// form.Rule{Path:"your.mom", In:"one, two, three"}
-
 func (rule Rule) IsEmpty() bool {
 	return (rule.Path == "")
 }
 
 func (rule Rule) HyperscriptRules() string {
 
-	var b strings.Builder
+	return ""
 
-	b.WriteString("on change send checkFormRules(changed:me as Values) end ")
+	/*
+		var b strings.Builder
+		b.WriteString("on load log 'here' send go end on change send go end on go log me send checkFormRules(changed:me as Values) end ")
 
-	if !rule.IsEmpty() {
-		b.WriteString("on checkFormRules(changed) from window ")
-		b.WriteString("set value to event.detail.changed['" + rule.Path + "'] if value is undefined then exit else if (value " + rule.Operator() + " " + rule.Value + ") then show me else hide me end ")
-	}
+		if !rule.IsEmpty() {
+			b.WriteString("on checkFormRules(changed) from window ")
+			b.WriteString("set value to event.detail.changed['" + rule.Path + "'] if value is undefined then exit else if (value " + rule.Operator() + " " + rule.Value + ") then show me else hide me end ")
+		}
 
-	return b.String()
+		return b.String()
+	*/
 }
