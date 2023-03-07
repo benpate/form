d77c7ac67116f49450d01c0079b404d5d26138c3
Ben Pate <ben@pate.org>
2023-03-06 17:29:37 -0700

    Add sort algorithms for LookupCodes

---
+++ sort.go
@@ -0,0 +1,38 @@
+package form
+
+import "math"
+
+func SortLookupCodeByLabel(a LookupCode, b LookupCode) bool {
+	return a.Label < b.Label
+}
+
+func SortLookupCodeByGroupThenLabel(a LookupCode, b LookupCode) bool {
+	if a.Group == b.Group {
+		return a.Label < b.Label
+	}
+	return a.Group < b.Group
+}
+
+func SortLookupCodesBySelectedValues(selected []string) func(a LookupCode, b LookupCode) bool {
+
+	indexOf := func(slice []string, value string) int {
+		for index, item := range slice {
+			if item == value {
+				return index
+			}
+		}
+		return math.MaxInt
+	}
+
+	return func(a LookupCode, b LookupCode) bool {
+
+		aIndex := indexOf(selected, a.Value)
+		bIndex := indexOf(selected, b.Value)
+
+		if aIndex == bIndex {
+			return false
+		}
+
+		return aIndex < bIndex
+	}
+}
