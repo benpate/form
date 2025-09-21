7367f00b5daab569cd04dbbdabb66373efdcd345
Ben Pate <ben@pate.org>
2025-09-21 12:34:33 -0600

    Improve nil checks

--- registry.go
+++ registry.go
@@ -2,11 +2,7 @@ package form
 
 // registry is the system-wide registry of all form widgets.
 // it must be populated with all available widgets before the system can be used.
-var registry map[string]Widget
-
-func init() {
-	registry = make(map[string]Widget)
-}
+var registry map[string]Widget = make(map[string]Widget)
 
 // Use adds a new widget into the widget registry.
 func Use(name string, widget Widget) {
