42ec58c34bd14a8a0e980c7a7c0335a0f67f196a
Ben Pate <ben@pate.org>
2025-10-28 11:41:17 -0600

    LookupCode accessors
    - define new GetStringOK and SetString data accessors for LookupCodes

--- lookupCode_accessors.go
+++ lookupCode_accessors.go
@@ -41,3 +41,63 @@ func (lookupCode *LookupCode) GetPointer(name string) (any, bool) {
 
 	return nil, false
 }
+
+func (lookupCode LookupCode) GetStringOK(name string) (string, bool) {
+
+	switch name {
+
+	case "value":
+		return lookupCode.Value, true
+
+	case "label":
+		return lookupCode.Label, true
+
+	case "description":
+		return lookupCode.Description, true
+
+	case "icon":
+		return lookupCode.Icon, true
+
+	case "group":
+		return lookupCode.Group, true
+
+	case "href":
+		return lookupCode.Href, true
+
+	}
+
+	return "", false
+}
+
+func (lookupCode *LookupCode) SetString(name string, value string) bool {
+
+	switch name {
+
+	case "value":
+		lookupCode.Value = value
+		return true
+
+	case "label":
+		lookupCode.Label = value
+		return true
+
+	case "description":
+		lookupCode.Description = value
+		return true
+
+	case "icon":
+		lookupCode.Icon = value
+		return true
+
+	case "group":
+		lookupCode.Group = value
+		return true
+
+	case "href":
+		lookupCode.Href = value
+		return true
+
+	}
+
+	return false
+}
